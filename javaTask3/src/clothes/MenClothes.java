ab0c4254fc34ce80689deabf68a793f500eddb2a
Dmytro Sokolovskiy <72861157+DmytroSokolovskyi@users.noreply.github.com>
2021-02-28 11:39:12 +0200

    Delete javaTask3 directory

--- javaTask3/src/clothes/MenClothes.java
+++
@@ -1,5 +0,0 @@
-package clothes;
-
-public interface MenClothes {
-    public void toDressMan();
-}
--- javaTask3/src/clothes/WomenClothers.java
+++
@@ -1,5 +0,0 @@
-package clothes;
-
-public interface WomenClothers {
-    public void toDressWoman();
-}
