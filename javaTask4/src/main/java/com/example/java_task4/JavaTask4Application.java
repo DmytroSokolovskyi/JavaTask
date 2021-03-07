ee66fce090543ad42481a794d5087d9837387e37
Dmytro Sokolovskiy <72861157+DmytroSokolovskyi@users.noreply.github.com>
2021-03-07 10:11:46 +0200

    Add files via upload

---
+++ javaTask4/src/main/java/com/example/java_task4/JavaTask4Application.java
@@ -0,0 +1,13 @@
+package com.example.java_task4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTask4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTask4Application.class, args);
+    }
+
+}
