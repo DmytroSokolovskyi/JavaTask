7a579dec24997dbd2e417da08b5e427cefbbb067
Dmytro Sokolovskiy <72861157+DmytroSokolovskyi@users.noreply.github.com>
2021-03-14 18:57:22 +0200

    Add files via upload

---
+++ javatask8/src/main/java/com/example/javatask8/Javatask8Application.java
@@ -0,0 +1,13 @@
+package com.example.javatask8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javatask8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javatask8Application.class, args);
+    }
+
+}
